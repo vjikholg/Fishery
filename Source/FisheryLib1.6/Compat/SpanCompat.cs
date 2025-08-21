f3e74176e0f3185c1f3a9ee2e67e5bda294a7431
vjikholg <yuvict1@outlook.com>
2025-08-20 22:27:52 -0700

    fixed SpanCompat referencing FisheryLib.MemoryMarshal rather than System.Runtime.InteropServices.MemoryMarshal, which is necessary since they've moved DangerousGetPinnableReference Span/ReadOnlySpan methods to inside MemoryMarshal. https://github.com/dotnet/runtime/issues/24207

--- Source/FisheryLib1.6/Compat/SpanCompat.cs
+++ Source/FisheryLib1.6/Compat/SpanCompat.cs
@@ -3,14 +3,19 @@
 // License, v.2.0.If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+extern alias HarmonyAlias;
+
 using System.Runtime.CompilerServices;
+using MM = HarmonyAlias::System.Runtime.InteropServices.MemoryMarshal;
+
 
 namespace FisheryLib;
-
+#if V1_6
 public static class SpanCompat
 {
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public static ref T DangerousGetPinnableReference<T>(this ReadOnlySpan<T> span) => ref MemoryMarshal.GetReference(span);
+	public static ref T DangerousGetPinnableReference<T>(this ReadOnlySpan<T> span) => ref MM.GetReference(span); // FLAG AS CRASHCODE LMAO.
 
 	public static ref T DangerousGetPinnableReference<T>(this Span<T> span) => ref span.GetPinnableReference();
 }
+#endif
